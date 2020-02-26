e663bf8b4c4db79a2f710618666b53fb4da1099b
jakmol@ug.kth.se <MmR-PZ-Q>
2020-02-26 13:38:54 +0100

    test db

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -12,7 +12,7 @@ async function asyncFunction() {
   try {
 	conn = await pool.getConnection();
 	const rows = await conn.query("SELECT * FROM User");
-	console.log(rows); //[ {val: 1}, meta: ... ]
+	console.log(rows[0]); //[ {val: 1}, meta: ... ]
 	// const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
     // console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
     
