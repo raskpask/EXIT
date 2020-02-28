77e6fd3bc726af4daf87bdbba6317c3b23e2eaf7
rikjak <=>
2020-02-28 15:27:58 +0100

    Update userDAO.js

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -202,8 +202,8 @@ function registerProject(project_details){
                 }
                 await client.query(addCompanyQuery.text,addCompanyQuery.values)
                 .then(res=> {
-                    console.log(Object.keys(res[1][0])[0]);
-                    project_details.company = Object.keys(res[1][0])[0];
+                    console.log(Object.keys(res[1][0])[1]);
+                    project_details.company = Object.keys(res[1][0])[1];
                 })
                 .catch(err=>{
                     client.query("ROLLBACK");
