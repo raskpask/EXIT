6e2ccd7128b09046e9edd6fce62ec7da9b3f38b9
rikjak <=>
2020-02-28 12:44:13 +0100

    Update userDAO.js

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -214,8 +214,9 @@ function registerProject(project_details){
         }
     });
 }
-
-registerProject(new ProjectDetails(null,1,"testprojekt",15,"2020-01-05","2020-06-01",1,0,1,null,1,"testföretaget","test","1234565"));
+getUser(1);
+getProject(1);
+//registerProject(new ProjectDetails(null,1,"testprojekt",15,"2020-01-05","2020-06-01",1,0,1,null,1,"testföretaget","test","1234565"));
 
 
 module.exports = {
