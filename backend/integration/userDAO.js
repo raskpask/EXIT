5b5876d1dab8681e8ed2d2a4ef3f11cab31f635c
rikjak <=>
2020-02-28 15:41:02 +0100

    Made database functions work
    Made registerProject, getUser and getProject work.

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -226,8 +226,8 @@ function registerProject(project_details){
         }
     });
 }
-console.log(getUser(1));
-console.log(getProject(2));
+console.log( await getUser(1));
+console.log( await getProject(2));
 registerProject(new ProjectDetails(null,1,"testprojekt",15,"2020-01-05","2020-06-01",1,0,1,null,1,"testförefdataget","tesasdt","1sdf234565"));
 
 
