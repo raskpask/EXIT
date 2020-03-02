64a6f794ad543e179fec058213937d90480c54a5
jakmol@ug.kth.se <MmR-PZ-Q>
2020-03-02 15:32:34 +0100

    added endpoints

--- backend/net/apiEndpoint.js
+++ backend/net/apiEndpoint.js
@@ -32,13 +32,20 @@ function router(router) {
     router.get('/api/user', async (req, res) => {
         try {
             const user = await controller.getUser(req);
-            res.cookie('privilegeLevel', user.privilegeLevel, { expires: new Date(Date.now() + 1800000) });
-            res.cookie('authToken', controller.getToken(req), { expires: new Date(Date.now() + 1800000) });
             res.send(JSON.stringify({ user: user }));
         } catch (error) {
             dbErrors.respondError(error.message, res)
             console.error(error)
         }
+    });
+    router.delete('/api/user', async (req, res) => {
+        try {
+            // const statusCode = await controller.registerUser(req);
+            res.status(500);
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+        res.send();
     });
 
     router.post('/api/project', async (req, res) => {
@@ -61,89 +68,71 @@ function router(router) {
         }
         //res.send();
     });
-
-    router.get('/api/username', async (req, res) => {
-        try {
-            res.send(await controller.checkIfUsernameIsAvailable(req));
-        } catch (e) {
-            dbErrors.respondError(e.message, res)
-            console.error(e);
-            res.sendStatus(500);
-        }
-    });
-
-
-    router.post('/api/authentication', async (req, res) => {
-        try {
-            const user = await controller.authenticateUser(req);
-            const lang = await controller.extractLangCookie(req);
-            res.cookie('lang',lang);
-            res.cookie('authToken', user.token, { expires: new Date(Date.now() + 1800000) });
-            res.cookie('privilegeLevel', user.privilegeLevel, { expires: new Date(Date.now() + 1800000) });
-        } catch (error) {
-            dbErrors.respondError(error.message, res)
-            console.error(error);
-            res.status(401);
-        }
-        res.send()
-    });
-
-    router.delete('/api/authentication', async (req, res) => {
-        try {
-            await controller.deAuthenticateUser(req);
-            res.clearCookie('authToken');
-            res.clearCookie('privilegeLevel')
-            res.send();
-        } catch (error) {
-            dbErrors.respondError(error.message, res)
-            console.error(error)
-            res.status(401);
-        }
-    })
-
-    router.get('/api/application', async (req, res) => {
-        try {
-            const application = await controller.getApplication(req);
-            if (application == "no access") {
-                res.sendStatus(401)
-            } else {
-                res.send(JSON.stringify(application));
-            }
-        } catch (error) {
-            dbErrors.respondError(error.message, res)
-            console.error(error);
-        }
-    });
-
-    router.post('/api/application', async (req, res) => {
-        try {
-            const application = await controller.createApplication(req);
-            res.send("Application was created");
+    router.get('/api/expertise', async (req, res) => {
+        try {    
+        // Fects the expertise of the user
+        res.send(500)
         } catch (error) {
             dbErrors.respondError(error.message, res)
         }
     });
-
-    router.put('/api/application', async (req, res) => {
-        try {
-            const application = await controller.updateApplicationStatus(req);
-            if (application === 200) {
-                res.send()
-            }
+    router.post('/api/expertise', async (req, res) => {
+        try {    
+        // Adds the expertise to user
+        res.send(500)
         } catch (error) {
             dbErrors.respondError(error.message, res)
         }
     });
-
-    router.get('/api/competence', async (req, res) => {
-        try {
-            res.send(JSON.stringify(await controller.getCompetence(req)));
+    router.put('/api/expertise', async (req, res) => {
+        try {    
+        // Updates the expertise of the user
+        res.send(500)
         } catch (error) {
             dbErrors.respondError(error.message, res)
-            console.error(error)
         }
-    })
-}
+    });
+    router.delete('/api/expertise', async (req, res) => {
+        try {    
+        // Deletes the expertise of the user
+        res.send(500)
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+    });
+    router.get('/api/budgetYear', async (req, res) => {
+        try {    
+        // Send a budget year
+        res.send(500)
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+    });
+    router.post('/api/budgetYear', async (req, res) => {
+        try {    
+        // Add a budget year
+        res.send(500)
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+    });
+    router.put('/api/budgetYear', async (req, res) => {
+        try {    
+        // Update a budget year
+        res.send(500)
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+    });
+    router.delete('/api/budgetYear', async (req, res) => {
+        try {    
+        // Delete a budget year
+        res.send(500)
+        } catch (error) {
+            dbErrors.respondError(error.message, res)
+        }
+    });
+    
 
 module.exports = {
     router,
