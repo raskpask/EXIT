48f23d1a1f0a32c4cdb0dac918f61490f2f4b4b2
jakmol@ug.kth.se <MmR-PZ-Q>
2020-03-02 17:03:14 +0100

    post budget year

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -240,7 +240,7 @@ function postBudgetYear(budget_year) {
         const client = await pool.getConnection()
         let postBudgetYear = {
             text: "INSERT INTO Budget_year (year,master_hours,bachelor_hours,total_tutoring_hours,factor_1,factor_2,factor_3,factor_4,factor_5) " +
-            "VALUES (?,?,?,?,?,?,?,?)",
+            "VALUES (?,?,?,?,?,?,?,?,?)",
             values: [budget_year.year,budget_year.master_hours,budget_year.bachelor_hours,budget_year.total_tutoring_hours,
                 budget_year.factor_1,budget_year.factor_2,budget_year.factor_3,budget_year.factor_4,budget_year.factor_5]
         }
