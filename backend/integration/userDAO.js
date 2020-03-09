fac204b6a5344da12b433fd47940ba70c1aa2da8
jakmol@ug.kth.se <MmR-PZ-Q>
2020-03-09 14:33:43 +0100

    get project

--- backend/integration/userDAO.js
+++ backend/integration/userDAO.js
@@ -336,7 +336,7 @@ function getProject(user_id, year) {
                             .then(res => {
                                 const users = JSON.stringify(res).split("meta")[0]+']'
                                 projects.push(JSON.stringify(new ProjectDetails(project.project_id, project.number_of_students, project.title, project.project_description, project.credits, project.start_date, project.end_date, res.in_progress, project.out_of_date, project.all_info_specified, project.company, project.company_contact, project.name, project.address, project.phone_number, users)))
-                                // console.log(projects)
+                                console.log(projects)
                             })
                             .catch(err => {
                                 console.error(err)
