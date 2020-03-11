e6afb2729652d5b6af90f7a2f48d9c02b746a670
jakmol@ug.kth.se <MmR-PZ-Q>
2020-03-11 10:40:38 +0100

    login

--- backend/net/CASLogin.js
+++ backend/net/CASLogin.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const sp_options = {
     entity_id: "https://samltest.sys.kth.se/idp/shibboleth",
     // private_key: fs.readFileSync("key-file.pem").toString(),
-    // certificate: "https://mds.swamid.se/md/md-signer2.crt",
+    certificate: "MIIDPzCCAiegAwIBAgIUScUSOrMmb/KzMd1Muhpc5sElLeswDQYJKoZIhvcNAQEL BQAwIDEeMBwGA1UEAwwVc2FtbHRlc3QtNS5zeXMua3RoLnNlMB4XDTE3MTAyNTEx Mzk0MFoXDTM3MTAyNTEyMzk0MFowIDEeMBwGA1UEAwwVc2FtbHRlc3QtNS5zeXMu a3RoLnNlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8q6XIIiftB1G 2I//JwVR8qRBbhfdzzGHbCQr5lFrUXYV89dmsk6E2Nr9rQP+yrYZU/eIDJ4XBQdI oIWe8u1ImtPWjzTHVXd5z2SmRJ98EFFZtju2mCLqqD4qUcNl3DbaPXgqy5Hm5ohV mEQlxO1jO7xq/f4n6d72noQCWTfYdXOZwD1voEc4Hox2AWP+tPx0FLRSqj3h+44s elVwEObb8pxQEYmsfxVnBm2i/aOBLlsntaLQhCFjs5umLd4UDP9lvIwJYx460QX6 PyHAYDMA8h9VNO/jfKp97JwtKf0Yrh3RUJ210MNkYcV5N8JBUX+KGT7fYI6FN9OX +QTXupKPQwIDAQABo3EwbzAdBgNVHQ4EFgQUvyWzocZiagOIj4tWholK1IuwGSMw TgYDVR0RBEcwRYIVc2FtbHRlc3QtNS5zeXMua3RoLnNlhixodHRwczovL3NhbWx0 ZXN0LTUuc3lzLmt0aC5zZS9pZHAvc2hpYmJvbGV0aDANBgkqhkiG9w0BAQsFAAOC AQEA64IorZp5656GLTNnlBFEvegWYPQE3GW+tDMJ40lzzhY+QSpM1FIxggF2YJsT VhEWNqITZz1lcKjEycBaR8kEDatg0fgjhXrwGwVKIOqU+bZTZF5Ez8r0d9Y0DYSe kGSKCdIYung1ylHcuCFC79Ls197OHkLRwn0DNoNEld9bHdE+keipdq5oR5xjZeys cNcwYx6ss2ody6XieevUCsbMhHfzniA1V2ccK8dAmzoLCkqoeIN8HISbOCh+JN2x Iol9Vr1XBYMb8c7q8mIK7Jo8iZy3lzGL15YHnVRGw0lEIFnbyw+hs9kiKtAavwZj dYVmg1bH4O/srfw2WyZikn6tCg==",
     assert_endpoint: "https://130.237.202.87:8080/assert",
 }
 const idp_options = {
