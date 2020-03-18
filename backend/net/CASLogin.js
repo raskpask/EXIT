b6605e5e063228ce19e467a3e152fa9751ee274d
jakmol@ug.kth.se <MmR-PZ-Q>
2020-03-18 10:22:48 +0100

    logout

--- backend/net/CASLogin.js
+++ backend/net/CASLogin.js
@@ -128,10 +128,10 @@ function router(router) {
             const username = req.headers.cookie.split('username=')[1].split(';')[0]
             await controller.logout(username)
 
-            res.cookie('session_id', {expires: Date.now()});
-            res.cookie('role_id', {expires: Date.now()});
-            res.cookie('username', {expires: Date.now()});
-            res.cookie('name_id', {expires: Date.now()});
+            res.cookie('session_id', ' ', {expires: Date.now()});
+            res.cookie('role_id',' ', {expires: Date.now()});
+            res.cookie('username',' ', {expires: Date.now()});
+            res.cookie('name_id',' ', {expires: Date.now()});
             res.redirect('/')
 
         } catch (err) {
